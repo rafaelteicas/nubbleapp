4ce836e004c81574f9bab5cb7092e13a6ff7493a
Rafael <rafael@Rafaels-iMac-Pro.local>
2023-11-22 12:12:45 -0300

    chore: init React Native app

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <View style={{backgroundColor: "orange", width: '100%', height: '100%'}}>
+        <Text>sadkaskd</Text>
+      </View>
+    )
+  }
+}
+
+export default App
