b233f63626d8b70be60a9950cdaec41ffff40582
Rafael <rafaelteicas@gmail.com>
2023-11-23 19:59:21 -0300

    feat: create Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Text} from '../Text/Text';
+import {TouchableOpacityBox, TouchableOpacityBoxProps} from '../Box/Box';
+import {ActivityIndicator} from '../ActivityIndicator/ActivityIndicator';
+import {ButtonPreset, buttonPresets} from './buttonPresets';
+
+interface ButtonProps extends TouchableOpacityBoxProps {
+  title?: string;
+  loading?: boolean;
+  preset?: ButtonPreset;
+  disabled?: boolean;
+}
+
+export function Button({
+  title,
+  preset = 'primary',
+  loading,
+  disabled = false,
+  ...touchableOpacityBoxProps
+}: ButtonProps) {
+  const buttonPreset = buttonPresets[preset][disabled ? 'disabled' : 'default'];
+  return (
+    <TouchableOpacityBox
+      paddingHorizontal="s20"
+      height={50}
+      alignItems="center"
+      justifyContent="center"
+      borderRadius="s16"
+      {...buttonPreset.container}
+      {...touchableOpacityBoxProps}>
+      {loading ? (
+        <ActivityIndicator color={buttonPreset.content} />
+      ) : (
+        <Text bold preset="paragraphMedium" color={buttonPreset.content}>
+          {title}
+        </Text>
+      )}
+    </TouchableOpacityBox>
+  );
+}
