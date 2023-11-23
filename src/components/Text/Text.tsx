e66ebc58f2ee95766f34e6bf6ea50ae71448a394
Rafael <rafaelteicas@gmail.com>
2023-11-23 19:59:03 -0300

    feat: create Text component

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,88 @@
+import {createText} from '@shopify/restyle';
+import React from 'react';
+import {Theme} from '../../theme/theme';
+
+const SRText = createText<Theme>();
+type SRTextProps = React.ComponentProps<typeof SRText>;
+
+export interface TextProps extends SRTextProps {
+  preset?: TextVariants;
+  bold?: boolean;
+  semiBold?: boolean;
+  italic?: boolean;
+}
+
+export function Text({
+  children,
+  style,
+  bold,
+  semiBold,
+  italic,
+  preset = 'paragraphMedium',
+  ...sRTextProps
+}: TextProps) {
+  const fontFamily = getFontFamily(preset, bold, italic, semiBold);
+  return (
+    <SRText
+      color="backgroundContrast"
+      style={[$fontSizes[preset], {fontFamily}, style]}
+      {...sRTextProps}>
+      {children}
+    </SRText>
+  );
+}
+
+function getFontFamily(
+  preset?: string,
+  bold?: boolean,
+  semiBold?: boolean,
+  italic?: boolean,
+) {
+  if (preset?.includes('heading')) {
+    return italic ? $fontFamily.boldItalic : $fontFamily.bold;
+  }
+  switch (true) {
+    case bold && italic:
+      return $fontFamily.boldItalic;
+    case bold:
+      return $fontFamily.bold;
+    case semiBold && italic:
+      return $fontFamily.mediumItalic;
+    case semiBold:
+      return $fontFamily.medium;
+    case italic:
+      return $fontFamily.italic;
+  }
+}
+
+type TextVariants =
+  | 'headingLarge'
+  | 'headingMedium'
+  | 'headingSmall'
+  | 'paragraphLarge'
+  | 'paragraphMedium'
+  | 'paragraphSmall';
+
+const $fontSizes: Record<TextVariants, {fontSize: number; lineHeight: number}> =
+  {
+    headingLarge: {fontSize: 32, lineHeight: 38.4},
+    headingMedium: {fontSize: 22, lineHeight: 26.4},
+    headingSmall: {fontSize: 18, lineHeight: 23.4},
+
+    paragraphLarge: {fontSize: 18, lineHeight: 25.2},
+    paragraphMedium: {fontSize: 16, lineHeight: 22.4},
+    paragraphSmall: {fontSize: 14, lineHeight: 19.6},
+  };
+
+const $fontFamily = {
+  black: 'Satoshi-Black',
+  blackItalic: 'Satoshi-BlackItalic',
+  bold: 'Satoshi-Bold',
+  boldItalic: 'Satoshi-BoldItalic',
+  italic: 'Satoshi-Italic',
+  light: 'Satoshi-Light',
+  lightItalic: 'Satoshi-LightItalic',
+  medium: 'Satoshi-Medium',
+  mediumItalic: 'Satoshi-MediumItalic',
+  regular: 'Satoshi-Regular',
+};
