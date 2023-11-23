5d78f7e268ca22d37d3bfca9eb7af80334c6dbf7
Rafael <rafaelteicas@gmail.com>
2023-11-23 19:59:10 -0300

    feat: create ActivityIndicator component

---
+++ src/components/ActivityIndicator/ActivityIndicator.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  ActivityIndicatorProps,
+  ActivityIndicator as RNActivityIndicator,
+} from 'react-native';
+import {Theme, ThemeColors} from '../../theme/theme';
+import {useTheme} from '@shopify/restyle';
+
+interface Props extends Omit<ActivityIndicatorProps, 'color'> {
+  color: ThemeColors;
+}
+
+export function ActivityIndicator({color, ...activityIndicatorProps}: Props) {
+  const {colors} = useTheme<Theme>();
+  return (
+    <RNActivityIndicator color={colors[color]} {...activityIndicatorProps} />
+  );
+}
