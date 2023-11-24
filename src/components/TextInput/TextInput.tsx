8a888d1cbaaf4c36da464d4611f7a1e40771a0c9
Rafael <rafaelteicas@gmail.com>
2023-11-24 17:50:28 -0300

    feat: create SignUpScreen

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -9,7 +9,7 @@ import {$fontFamily, $fontSizes, Text} from '../Text/Text';
 import {Box, BoxProps} from '../Box/Box';
 import {useAppTheme} from '../../hooks/useAppTheme';
 
-interface TextInputProps extends RNTextInputProps {
+export interface TextInputProps extends RNTextInputProps {
   label: string;
   errorMessage?: string;
   RightComponent?: React.ReactElement;
