fdbfbc3a21144e1b0952c07c18f30255aac87556
Rafael <rafaelteicas@gmail.com>
2023-11-24 17:50:07 -0300

    feat: create Screen component

---
+++ src/components/Screen/components/ScreenContainer.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {ScrollView, View} from 'react-native';
+
+type Props = {
+  children: React.ReactNode;
+  backgroundColor: string;
+};
+
+export function ScrollViewContainer({children, backgroundColor}: Props) {
+  return (
+    <ScrollView
+      keyboardShouldPersistTaps="handled"
+      style={{backgroundColor, flex: 1}}>
+      {children}
+    </ScrollView>
+  );
+}
+
+export function ViewContainer({children, backgroundColor}: Props) {
+  return <View style={{backgroundColor, flex: 1}}>{children}</View>;
+}
