8e23963846eeda72c98bb34f653db6b126ff482b
Rafael <rafaelteicas@gmail.com>
2023-11-26 14:30:08 -0300

    feat: create SuccessScreen

---
+++ src/screens/app/SuccessScreen/SuccessScreen.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Screen} from '../../../components/Screen/Screen';
+import {Icon} from '../../../components/Icon/Icon';
+import {Text} from '../../../components/Text/Text';
+import {Button} from '../../../components/Button/Button';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../../routes/Routes';
+
+type ScreenProps = NativeStackScreenProps<RootStackParamList, 'SuccessScreen'>;
+
+export function SuccessScreen({route}: ScreenProps) {
+  function goBackToBegin() {
+    //TODO: navegar para tela de login
+  }
+  return (
+    <Screen>
+      <Icon {...route.params.icon} />
+      <Text preset="headingLarge" mt="s24">
+        {route.params.title}
+      </Text>
+      <Text preset="paragraphLarge" mt="s16">
+        {route.params.description}
+      </Text>
+      <Button onPress={goBackToBegin} title="Voltar ao incio" mt="s40" />
+    </Screen>
+  );
+}
