8a7ea340bc95584970eefdef1a08d1efcf7794e6
Rafael <rafaelteicas@gmail.com>
2023-11-26 14:29:17 -0300

    feat: add routes and navigation to app

--- App.tsx
+++ App.tsx
@@ -2,14 +2,14 @@ import React, {Component} from 'react';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {SignUpScreen} from './src/screens/app/SignUpScreen/SignUpScreen';
+import {Routes} from './src/routes/Routes';
 
 export class App extends Component {
   render() {
     return (
       <SafeAreaProvider>
         <ThemeProvider theme={theme}>
-          <SignUpScreen />
+          <Routes />
         </ThemeProvider>
       </SafeAreaProvider>
     );
---
+++ src/routes/Routes.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {LoginScreen} from '../screens/app/LoginScreen/LoginScreen';
+import {SignUpScreen} from '../screens/app/SignUpScreen/SignUpScreen';
+import {SuccessScreen} from '../screens/app/SuccessScreen/SuccessScreen';
+import {IconProps} from '../components/Icon/Icon';
+
+export type RootStackParamList = {
+  LoginScreen: undefined;
+  SignUpScreen: undefined;
+  SuccessScreen: {
+    title: string;
+    description: string;
+    icon: Pick<IconProps, 'name' | 'color'>;
+  };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          fullScreenGestureEnabled: true,
+        }}
+        initialRouteName="LoginScreen">
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+        <Stack.Screen name="SuccessScreen" component={SuccessScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
