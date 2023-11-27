1b2dd77225608b6d1d4dd43e8c9419ee3fb1eb6a
Rafael <rafaelteicas@gmail.com>
2023-11-27 14:03:45 -0300

    refactor: export files from index file

---
+++ src/screens/app/index.ts
@@ -0,0 +1,4 @@
+export * from './ForgotPasswordScreen/ForgotPasswordScreen';
+export * from './LoginScreen/LoginScreen';
+export * from './SignUpScreen/SignUpScreen';
+export * from './SuccessScreen/SuccessScreen';
