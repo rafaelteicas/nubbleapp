fb686d9338f46e50da05eb9ba8c59b31435fa106
Rafael <rafaelteicas@gmail.com>
2023-11-28 15:14:44 -0300

    feat: create AppStackNavigator with 2 screens

---
+++ src/screens/app/SettingsScreen/SettingsScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {Screen, Text} from '@components';
+
+export function SettingsScreen() {
+  return (
+    <Screen canGoBack>
+      <Text>Settings</Text>
+    </Screen>
+  );
+}
