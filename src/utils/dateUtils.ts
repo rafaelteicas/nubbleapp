0e3aadeb2ce7d4d8611af2413928d679a45cc7fb
Rafael <rafaelteicas@gmail.com>
2023-12-11 20:25:04 -0300

    refactor: fix dateUtils

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -4,6 +4,7 @@ function formatRelative(dateISO: string) {
   const date = parseISO(dateISO);
   const now = new Date();
   const diffInSeconds = differenceInSeconds(now, date);
+
   if (diffInSeconds < 60) {
     return `${diffInSeconds} s`;
   }
@@ -11,11 +12,11 @@ function formatRelative(dateISO: string) {
   if (diffInMinutes < 60) {
     return `${diffInMinutes} m`;
   }
-  const diffInHours = Math.round(diffInMinutes / 24);
+  const diffInHours = Math.round(diffInMinutes / 60);
   if (diffInHours < 24) {
     return `${diffInHours} h`;
   }
-  const diffInWeeks = Math.round(diffInHours / 7);
+  const diffInWeeks = Math.round(diffInHours / 24);
   if (diffInWeeks < 4) {
     return `${diffInWeeks} sem`;
   }
