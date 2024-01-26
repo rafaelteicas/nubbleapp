1d5d9886a30b57932403e9df12bf8bb3fa0fdefc
Rafael <rafaelteicas@gmail.com>
2024-01-26 20:51:44 -0300

    test: testing postBottom component, ensure that the navigation will be called with correct values

--- src/components/PostItem/components/__tests__/PostBottom.test.tsx
+++ src/components/PostItem/components/__tests__/PostBottom.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {Post} from '@domain';
-import {render, screen} from 'test-utils';
+import {fireEvent, render, screen} from 'test-utils';
 
 import {PostBottom} from '../PostBottom';
 
@@ -19,10 +19,40 @@ const mockedPost: Post = {
     userName: 'mariajulia',
   },
 };
+
+const mockedNavigate = jest.fn();
+
+jest.mock('@react-navigation/native', () => {
+  const originalModule = jest.requireActual('@react-navigation/native');
+  return {
+    ...originalModule,
+    useNavigation: () => ({
+      navigate: mockedNavigate,
+    }),
+  };
+});
+
 describe('<PostBottom />', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
   it('does not show the comment link if it has no comment', () => {
     render(<PostBottom {...mockedPost} commentCount={0} />);
     const commentLinkElement = screen.queryByText(/comentário/);
     expect(commentLinkElement).toBeNull();
+  });
+  it('navigates to PostCommentScreen when the comment link is pressed', () => {
+    render(<PostBottom {...mockedPost} commentCount={4} />);
+    const commentLinkElement = screen.getByText(/comentário/);
+    fireEvent.press(commentLinkElement);
+    expect(mockedNavigate).toHaveBeenCalledWith('PostCommentScreen', {
+      postId: mockedPost.id,
+      authorId: mockedPost.author.id,
+    });
+  });
+  it('should shows ver comentario if has just one comment', () => {
+    render(<PostBottom {...mockedPost} commentCount={1} />);
+    const commentLinkElement = screen.getByText(/ver comentário/);
+    expect(commentLinkElement).toBeTruthy();
   });
 });
